f009917e27f6c53cdc354654552c7dd8c4c4bcf0
pureitze <xsideflare@gmail.com>
2026-08-02 14:05:57 -0400

    Update DaggerKeys.java

--- src/main/java/com/example/daggers/DaggerKeys.java
+++ src/main/java/com/example/daggers/DaggerKeys.java
@@ -10,8 +10,12 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class DaggerKeys {
     public static NamespacedKey TYPE_KEY;
+    public static NamespacedKey TIER_KEY;
+    public static NamespacedKey UPGRADER_KEY;
 
     public static void init(JavaPlugin plugin) {
         TYPE_KEY = new NamespacedKey(plugin, "dagger_type");
+        TIER_KEY = new NamespacedKey(plugin, "dagger_tier");
+        UPGRADER_KEY = new NamespacedKey(plugin, "dagger_upgrader");
     }
 }
