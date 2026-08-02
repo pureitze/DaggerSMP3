43364a54093d2dc4359645d3b4741016e3bfc90b
pureitze <xsideflare@gmail.com>
2026-08-02 14:09:19 -0400

    Update DaggersPlugin.java

--- src/main/java/com/example/daggers/DaggersPlugin.java
+++ src/main/java/com/example/daggers/DaggersPlugin.java
@@ -3,9 +3,14 @@ package com.example.daggers;
 import com.example.daggers.listeners.DaggerAbilityListener;
 import com.example.daggers.listeners.DaggerDamageListener;
 import com.example.daggers.listeners.FreezeMovementListener;
+import com.example.daggers.listeners.RecipeGuiListener;
+import com.example.daggers.listeners.UpgraderListener;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
@@ -13,7 +18,6 @@ import java.util.Set;
 import java.util.UUID;
 
 public class DaggersPlugin extends JavaPlugin {
-
     private final CooldownManager cooldownManager = new CooldownManager();
     private final HitTracker hitTracker = new HitTracker();
     private final Set<UUID> pendingSoulCurse = new HashSet<>();
@@ -21,6 +25,10 @@ public class DaggersPlugin extends JavaPlugin {
     private FreezeManager freezeManager;
     private NoSprintManager noSprintManager;
     private DarknessInvisManager darknessInvisManager;
+    private RecipeManager recipeManager;
+    private RecipeGuiManager recipeGuiManager;
+    private Tier2BuffManager tier2BuffManager;
+    private GroundEffectManager groundEffectManager;
 
     @Override
     public void onEnable() {
@@ -28,17 +36,43 @@ public class DaggersPlugin extends JavaPlugin {
         freezeManager = new FreezeManager(this);
         noSprintManager = new NoSprintManager(this);
         darknessInvisManager = new DarknessInvisManager(this);
+        tier2BuffManager = new Tier2BuffManager(this);
+        groundEffectManager = new GroundEffectManager(this);
 
-  getServer().getPluginManager().registerEvents(
-          new DaggerDamageListener(pendingSoulCurse, pendingDarknessCurse, hitTracker, noSprintManager), this);
-  getServer().getPluginManager().registerEvents(
-                new DaggerAbilityListener(this, cooldownManager, freezeManager, pendingSoulCurse, pendingDarknessCurse, darknessInvisManager), this);
-  getServer().getPluginManager().registerEvents(
-          new FreezeMovementListener(freezeManager), this);
-  getServer().getPluginManager().registerEvents(
-          new com.example.daggers.listeners.NoSprintListener(noSprintManager), this);
+        recipeManager = new RecipeManager(this);
+        recipeManager.registerRecipes();
+        recipeGuiManager = new RecipeGuiManager(this, recipeManager);
+        getServer().getPluginManager().registerEvents(new RecipeGuiListener(recipeGuiManager), this);
+
+        registerUpgraderRecipe();
+
+        DaggerAbilityListener abilityListener = new DaggerAbilityListener(
+                this, cooldownManager, freezeManager, pendingSoulCurse, pendingDarknessCurse,
+                darknessInvisManager, tier2BuffManager, groundEffectManager
+        );
+        getServer().getPluginManager().registerEvents(abilityListener, this);
+
+        getServer().getPluginManager().registerEvents(
+                new DaggerDamageListener(pendingSoulCurse, pendingDarknessCurse, hitTracker, noSprintManager,
+                        tier2BuffManager, abilityListener), this);
+        getServer().getPluginManager().registerEvents(new FreezeMovementListener(freezeManager), this);
+        getServer().getPluginManager().registerEvents(new com.example.daggers.listeners.NoSprintListener(noSprintManager), this);
+        getServer().getPluginManager().registerEvents(new UpgraderListener(), this);
 
         getLogger().info("CustomDaggers enabled!");
+    }
+
+    private void registerUpgraderRecipe() {
+        NamespacedKey key = new NamespacedKey(this, "dagger_upgrader");
+        getServer().removeRecipe(key);
+
+        ShapedRecipe recipe = new ShapedRecipe(key, UpgraderItem.create());
+        recipe.shape("DND", "NBN", "DND");
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('N', Material.NETHERITE_INGOT);
+        recipe.setIngredient('B', Material.BEACON);
+
+        getServer().addRecipe(recipe);
     }
 
     @Override
@@ -60,8 +94,13 @@ public class DaggersPlugin extends JavaPlugin {
             return true;
         }
 
+        if (args.length == 1 && args[0].equalsIgnoreCase("recipe")) {
+            recipeGuiManager.openMainMenu(player);
+            return true;
+        }
+
         if (args.length != 1) {
-            player.sendMessage("§cUsage: /dagger <fire|ice|water|soul|darkness|backstab>");
+            player.sendMessage("§cUsage: /dagger <fire|ice|water|soul|darkness|backstab|recipe>");
             return true;
         }
 
@@ -69,7 +108,7 @@ public class DaggersPlugin extends JavaPlugin {
         try {
             type = DaggerType.valueOf(args[0].toUpperCase());
         } catch (IllegalArgumentException e) {
-            player.sendMessage("§cUnknown dagger. Choose: fire, ice, water, soul, darkness, backstab");
+            player.sendMessage("§cUnknown dagger. Choose: fire, ice, water, soul, darkness, backstab, recipe");
             return true;
         }
 
